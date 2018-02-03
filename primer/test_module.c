3cbbc549899f754ac358d850713dd2f416c25632
thakkarV <thakkarv@bu.edu>
2018-02-03 10:18:59 -0500

    test mod

---
+++ primer/test_module.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/config.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+
+static int __init initialization_routine(void)
+{
+	printk("Hello, world!\n");
+	return 0;
+}
+
+static void __exit cleanup_routine(void)
+{
+	printk("Unloading module!\n");
+}
+
+module_init(initialization_routine);
+module_exit(cleanup_routine);
