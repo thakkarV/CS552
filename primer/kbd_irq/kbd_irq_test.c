cdcd59aa2ef4e5a440e1cd609c76fd54a7bfec00
thakkarV <thakkarv@bu.edu>
2018-02-06 23:47:42 -0500

    irq wait based

--- primer/kbd_irq/kbd_irq_test.c
+++ primer/kbd_irq/kbd_irq_test.c
@@ -9,24 +9,11 @@ int main(int argc, char const * argv [])
 	// Test reading keys now
 	//
 	char c;
-	int i = 0;
-	int str_size = 4;
-	char * str = malloc(sizeof(char) * str_size);
+
 	while ((c = kbd_irq_getchar()) != '\n')
 	{
-		str[i++] = c;
-		if (i >= str_size)
-		{
-			str_size *= 2;
-			str = realloc(str, str_size);
-			if (!str)
-			{
-				printf("realloc error\n");
-				exit(1);
-			}
-		}
+		printf("Got %c \n", c);
 	}
 
-	printf("%s", str);
 	return 0;
 }
