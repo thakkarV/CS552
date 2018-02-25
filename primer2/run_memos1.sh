980a46eb2e5eb1cc0ecd1864a5b988c81f281ca7
thakkarV <thakkarv@bu.edu>
2018-02-25 09:54:59 -0500

    bug fixes. refactor. logic fixes. run shellfile.

---
+++ primer2/run_memos1.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+ASFILE="memos1"
+LDFILE="memos1"
+IMGFILE="memos1.img"
+MEMS_SIZE=8
+VNC_PORT=0
+
+as --32 $ASFILE.s -o $ASFILE.o
+ld -T $LDFILE.ld $ASFILE.o -o $ASFILE
+dd bs=1 if=$ASFILE of=$IMGFILE count=512
+qemu-system-i386 -hda $IMGFILE -m $MEMS_SIZE -vnc :$VNC_PORT
