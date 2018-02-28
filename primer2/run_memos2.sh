e78dec7c877eee97bead743875230cdfc5ec1cb2
Karanraj Chauhan <chauhank@bu.edu>
2018-02-28 02:58:39 -0500

    BUGFIX: replaced o with 0 in magic num. add template kernel. updated run script.

--- primer2/run_memos2.sh
+++ primer2/run_memos2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+KERNFILE="kernel"
 ASFILE="memos2"
 LDFILE="memos2"
 IMGFILE="memos2img"
 MNTDEST="/mnt/C"
 
+gcc -c $KERNFILE.c -o $KERNFILE.o -std=gnu99 -ffreestanding
 as --32 $ASFILE.S -o $ASFILE.o
-ld -T $LDFILE.ld $ASFILE.o -o $ASFILE
+ld -T $LDFILE.ld $ASFILE.o $KERNFILE.o -o memos2 -nostdlib
 mount ./$IMGFILE.img $MNTDEST -t ext2 -o loop,offset=32256
 cp memos2 $MNTDEST/boot
 unmount $MNTDEST
