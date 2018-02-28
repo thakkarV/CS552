68c9c46c44940fc7133689a2b0cccc96a9aca580
Karanraj Chauhan <chauhank@bu.edu>
2018-02-28 03:10:38 -0500

    BUGFIX: unmount command typo in run script

--- primer2/run_memos2.sh
+++ primer2/run_memos2.sh
@@ -11,5 +11,5 @@ as --32 $ASFILE.S -o $ASFILE.o
 ld -T $LDFILE.ld $ASFILE.o $KERNFILE.o -o memos2 -nostdlib
 mount ./$IMGFILE.img $MNTDEST -t ext2 -o loop,offset=32256
 cp memos2 $MNTDEST/boot
-unmount $MNTDEST
+umount $MNTDEST
 qemu-system-i386 -hda $IMGFILE.img
