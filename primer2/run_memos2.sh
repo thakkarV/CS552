c7fbc2657a46d65ef7a0a4041e3c1d01dd6ab0e6
Karanraj Chauhan <chauhank@bu.edu>
2018-02-28 19:05:30 -0500

    modified linker and run scripts for i686 binary

--- primer2/run_memos2.sh
+++ primer2/run_memos2.sh
@@ -6,10 +6,12 @@ LDFILE="memos2"
 IMGFILE="memos2img"
 MNTDEST="/mnt/C"
 
-gcc -c $KERNFILE.c -o $KERNFILE.o -std=gnu99 -ffreestanding
-as --32 $ASFILE.S -o $ASFILE.o
-ld -T $LDFILE.ld $ASFILE.o $KERNFILE.o -o memos2 -nostdlib
+i686-elf-as $ASFILE.S -o $ASFILE.o
+i686-elf-gcc $KERNFILE.c -o $KERNFILE.o -std=gnu99 -ffreestanding
+i686-elf-gcc -T $LDFILE.ld -o memos2 -nostdlib -ffreestanding $ASFILE.o $KERNFILE.o -lgcc
+
 mount ./$IMGFILE.img $MNTDEST -t ext2 -o loop,offset=32256
 cp memos2 $MNTDEST/boot
 umount $MNTDEST
+
 qemu-system-i386 -hda $IMGFILE.img
