0e5381884a87c0df2782fd9d5d8c71b6d3ede308
Karanraj Chauhan <chauhank@bu.edu>
2018-03-01 18:03:11 -0500

    added multiboot.h. changed <> to "". updated linker file output format. updated run script to use gcc

--- primer2/run_memos2.sh
+++ primer2/run_memos2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-KERNFILE="kernel"
-ASFILE="memos2"
+KERNFILE="gnu_example"
+ASMFILE="gnu_example"
 LDFILE="memos2"
 IMGFILE="memos2img"
 MNTDEST="/mnt/C"
 
-i686-elf-as $ASFILE.S -o $ASFILE.o
-i686-elf-gcc $KERNFILE.c -o $KERNFILE.o -std=gnu99 -ffreestanding
-i686-elf-gcc -T $LDFILE.ld -o memos2 -nostdlib -ffreestanding $ASFILE.o $KERNFILE.o -lgcc
+# gcc --32 $ASMFILE.S -o $ASMFILE.o
+gcc $ASMFILE.S $KERNFILE.c -o $KERNFILE.o -std=gnu99 -ffreestanding -m32 -nostdlib
+gcc -T $LDFILE.ld -o memos2 -nostdlib -ffreestanding $KERNFILE.o -lgcc
 
 mount ./$IMGFILE.img $MNTDEST -t ext2 -o loop,offset=32256
 cp memos2 $MNTDEST/boot
