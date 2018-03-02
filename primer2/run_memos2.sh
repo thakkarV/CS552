ff492e252a3345491710d5717d72d940d06e2c25
Karanraj Chauhan <chauhank@bu.edu>
2018-03-02 13:00:53 -0500

    renamed var to asmfile. cleaned up for submission

--- primer2/run_memos2.sh
+++ primer2/run_memos2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 KERNFILE="kernel"
-ASFILE="memos2"
+ASMFILE="memos2"
 LDFILE="memos2"
 IMGFILE="memos2img"
 MNTDEST="/mnt/C"
 
 gcc -c $KERNFILE.c -o $KERNFILE.o -std=gnu99 -ffreestanding
-as --32 $ASFILE.S -o $ASFILE.o
-ld -T $LDFILE.ld $ASFILE.o $KERNFILE.o -o memos2 -nostdlib
+as --32 $ASMFILE.S -o $ASMFILE.o
+ld -T $LDFILE.ld $ASMFILE.o $KERNFILE.o -o memos2 -nostdlib
 mount ./$IMGFILE.img $MNTDEST -t ext2 -o loop,offset=32256
 cp memos2 $MNTDEST/boot
 umount $MNTDEST
