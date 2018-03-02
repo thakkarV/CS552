1cfbc5d61782f5b42b4a041e6559d54b23a8e56e
thakkarv <thakkarv@bu.edu>
2018-03-02 08:48:49 -0500

    more fixes

--- primer2/run_memos2.sh
+++ primer2/run_memos2.sh
@@ -8,9 +8,9 @@ MNTDEST="/mnt/D"
 
 # gcc --32 $ASMFILE.S -o $ASMFILE.o
 gcc -march=i386 $ASMFILE.S $KERNFILE.c -o $KERNFILE.o -std=gnu99 -ffreestanding -m32 -nostdlib -lgcc -Wall -Wextra
-gcc -T $LDFILE.ld -o memos2 -nostdlib -ffreestanding $KERNFILE.o -lgcc -Wall -Wextra
+gcc -T $LDFILE.ld -o $KERNFILE.bin $KERNFILE.o -lgcc -Wall -Wextra -std=gnu99 -nostdlib -ffreestanding
 
 mount /root/host/CS552/primer2/$IMGFILE.img $MNTDEST -t ext2 -o loop,offset=32256
-cp /root/host/CS552/primer2/memos2 $MNTDEST/boot
+cp /root/host/CS552/primer2/$KERNFILE.bin $MNTDEST/boot
 sync
 qemu-system-i386 -hda $IMGFILE.img
