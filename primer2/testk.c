681c7ce650ac931b9bcd554b9f9b29d3907045bf
thakkarv <thakkarv@bu.edu>
2018-03-02 09:53:14 -0500

    trying things

--- primer2/testk.c
+++ primer2/testk.c
@@ -51,6 +51,7 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color);
 void
 cmain(void)
 {
+	return;
 	init();
 	cls();
 	return;
@@ -130,12 +131,12 @@ init(void)
 }
 
 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
+inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
 }
 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
+inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
