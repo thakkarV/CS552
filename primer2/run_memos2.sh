95ecdc410d328459d795eda0f6ac584013617006
Karanraj Chauhan <chauhank@bu.edu>
2018-03-01 23:17:20 -0500

    updated flags and disk name in run scipt

--- primer2/run_memos2.sh
+++ primer2/run_memos2.sh
@@ -3,15 +3,14 @@
 KERNFILE="gnu_example"
 ASMFILE="gnu_example"
 LDFILE="memos2"
-IMGFILE="memos2img"
-MNTDEST="/mnt/C"
+IMGFILE="disk"
+MNTDEST="/mnt/D"
 
 # gcc --32 $ASMFILE.S -o $ASMFILE.o
-gcc $ASMFILE.S $KERNFILE.c -o $KERNFILE.o -std=gnu99 -ffreestanding -m32 -nostdlib
-gcc -T $LDFILE.ld -o memos2 -nostdlib -ffreestanding $KERNFILE.o -lgcc
+gcc -march=i386 $ASMFILE.S $KERNFILE.c -o $KERNFILE.o -std=gnu99 -ffreestanding -m32 -nostdlib -lgcc -Wall -Wextra
+gcc -T $LDFILE.ld -o memos2 -nostdlib -ffreestanding $KERNFILE.o -lgcc -Wall -Wextra
 
-mount ./$IMGFILE.img $MNTDEST -t ext2 -o loop,offset=32256
-cp memos2 $MNTDEST/boot
-umount $MNTDEST
-
+mount /root/host/CS552/primer2/$IMGFILE.img $MNTDEST -t ext2 -o loop,offset=32256
+cp /root/host/CS552/primer2/memos2 $MNTDEST/boot
+sync
 qemu-system-i386 -hda $IMGFILE.img
