1dd2e4d600b7643935e1e997c9ee5261442c5bb2
Karanraj Chauhan <chauhank@bu.edu>
2018-04-05 18:10:16 -0400

    updated previously too low frequency. updated pit command for square wave generation

--- fifos/src/timer.c
+++ fifos/src/timer.c
@@ -5,9 +5,8 @@
 void
 init_pit(void)
 {
-	// TODO: check rate gen vs interrupt mode
-	/* channel 0, lo/hi access, rate gen, 16-bit mode -- 00110100 */
-	outb(PIT_COMMAND, 0x34);
+	/* channel 0, lo/hi access, sq wave, 16-bit mode -- 00110110 */
+	outb(PIT_COMMAND, 0x36);
 
 	/* send lo byte and hi byte to set up desired frequency */
 	outb(PIT_CHANNEL0_DATA, (PIT_BASE_FRQ / PIT_INT_FRQ) & 0xFF);
