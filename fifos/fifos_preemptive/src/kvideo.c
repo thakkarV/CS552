86df6ae58787a9f75c79053663c5e6b323971ab5
thakkarv <thakkarv@bu.edu>
2018-04-12 17:44:18 -0400

    added support for 4-bit curses color support through printf

--- fifos/fifos_preemptive/src/kvideo.c
+++ fifos/fifos_preemptive/src/kvideo.c
@@ -3,13 +3,13 @@
 /* Macros */
 /* Some screen stuff. */
 /* The number of columns. */
-#define COLUMNS                 80
+#define COLUMNS 80
 /* The number of lines. */
-#define LINES                   24
-/* The attribute of an character. */
-#define ATTRIBUTE               7
+#define LINES 25
 /* The video memory address. */
-#define VIDEO                   0xB8000
+#define VIDEO 0xB8000
+/* The attribute of an character. */
+static char ATTRIBUTE = 0x07;
 
 /* Variables. */
 /* Save the X position. */
@@ -18,6 +18,28 @@ static int xpos;
 static int ypos;
 /* Point to the video memory. */
 static volatile unsigned char *video;
+
+static void __terminal_change_attrib(int code);
+
+
+/* COLOR MAP */
+#define DBLACK    0x0
+#define DBLUE     0x1
+#define DGREEN    0x2
+#define DCYAN     0x3
+#define DRED      0x4
+#define DPINK     0x5
+#define ORANGE    0x6
+#define LGREY     0x7
+#define DGREY     0x8
+#define LBLUE     0x9
+#define LGREEN    0xA
+#define LCYAN     0xB
+#define LRED      0xC
+#define LPINK     0xD
+#define YELLOW    0xE
+#define WHITE     0xF
+
 
 /* Clear the screen and initialize VIDEO, XPOS and YPOS. */
 void
@@ -41,6 +63,12 @@ putchar (int c)
 	if (c == '\n' || c == '\r')
 	{
 		newline:
+		while (xpos < COLUMNS)
+		{
+			*(video + (xpos + ypos * COLUMNS) * 2) = ' ' & 0xFF;
+			xpos++;
+		}
+
 		xpos = 0;
 		ypos++;
 		if (ypos >= LINES)
@@ -70,8 +98,28 @@ printf (const char *format, ...)
 
 	while ((c = *format++) != 0)
 	{
-		if (c != '%')
+		if (c == '\033')
+		{
+			if ((c = *format++) == '[')
+			{
+				int code;
+				while (c != 'm')
+				{
+					code = 0;
+					c = *format++;
+					while ((c != ';') && (c != 'm'))
+					{
+						code = (code << 3) + (code << 1) + (c - '0');
+						c = *format++;
+					}
+					__terminal_change_attrib(code);
+				}
+			}
+		}
+		else if (c != '%')
+		{
 			putchar (c);
+		}
 		else
 		{
 			char *p, *p2;
@@ -169,5 +217,41 @@ itoa (char *buf, int base, int d)
 		*p2 = tmp;
 		p1++;
 		p2--;
+	}
+}
+
+
+void
+__terminal_change_attrib(int code)
+{
+	static const char const COLOR_LUT[16] = 
+	{
+		DBLACK, DRED,  DGREEN, ORANGE,
+		DBLUE,  DPINK, DCYAN,  LGREY,
+		DGREY,  LRED,  LGREEN, YELLOW,
+		LBLUE,  LPINK, LCYAN,  WHITE
+	};
+
+	/* background is the top 4 MSBs, foreground is the bottom 4 LSBs */
+
+	// FG COLOR DARK
+	if (code >= 30 && code <= 37)
+	{
+		ATTRIBUTE = (ATTRIBUTE & 0xF0) + COLOR_LUT[code - 30];
+	}
+	// BG COLOR DARK
+	else if (code >= 40 && code <= 47)
+	{
+		ATTRIBUTE = (ATTRIBUTE & 0x0F) + (COLOR_LUT[code - 40] << 4);
+	}
+	// FG COLOR LIGHT
+	else if (code >= 90 && code <= 97)
+	{
+		ATTRIBUTE = (ATTRIBUTE & 0xF0) + COLOR_LUT[code - 82];
+	}
+	// BG COLOR LIGHT
+	else if (code >= 100 && code <= 107)
+	{
+		ATTRIBUTE = (ATTRIBUTE & 0x0F) + (COLOR_LUT[code - 92] << 4);
 	}
 }
