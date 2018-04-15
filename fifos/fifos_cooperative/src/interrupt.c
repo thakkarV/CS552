c8ceb8c135e53770d6fceda11a2989627d5165de
thakkarv <thakkarv@bu.edu>
2018-04-15 13:21:32 -0400

    Full exception support for ISR 0 to 16 for debug
    Adds proper error reporting of faults, traps and exceptions in the ISR 0
    to ISR 16 range for better debugging support

--- fifos/fifos_cooperative/src/interrupt.c
+++ fifos/fifos_cooperative/src/interrupt.c
@@ -6,7 +6,8 @@
 
 static struct IDTR idtr;
 static struct IDTDesc IDT[ISR_COUNT];
-static long jiffies;
+
+extern void do_timer(void);
 
 void
 init_pic(void)
@@ -24,17 +25,11 @@ init_pic(void)
 
 	/* ICW3 - tell Slave PIC its cascade identity */
 	outb(PIC2_DATA, 0x2);
-}
 
-
-void
-eoi(uint8_t irq)
-{
-	if (irq >= 8)
-		outb(PIC2_CTRL, EOI);
-	else
-		outb(PIC1_CTRL, EOI);
-	printf("eoi\n");
+	for (int i = 1; i < 16; ++i)
+	{
+		irq_mask(i);
+	}
 }
 
 
@@ -49,23 +44,23 @@ init_idt(void)
 	// clear IDT and ensure all zeros
 	memset(&IDT, 0x0, sizeof(struct IDTDesc) * ISR_COUNT);
 
-	set_trap_gate(  0, &isr_unimpl);
-	set_trap_gate(  1, &isr_unimpl);
-	set_trap_gate(  2, &isr_unimpl);
-	set_system_gate(3, &isr_unimpl);
-	set_system_gate(4, &isr_unimpl);
-	set_system_gate(5, &isr_unimpl);
-	set_trap_gate(  6, &isr_unimpl);
-	set_trap_gate(  7, &isr_unimpl);
+	set_trap_gate(  0, &isr_divide_error);
+	set_trap_gate(  1, &isr_debug);
+	set_trap_gate(  2, &isr_nmi);
+	set_system_gate(3, &isr_int3);
+	set_system_gate(4, &isr_overflow);
+	set_system_gate(5, &isr_bounds);
+	set_trap_gate(  6, &isr_invalid_op);
+	set_trap_gate(  7, &isr_device_unavailable);
 	set_trap_gate(  8, &isr_double_fault);
-	set_trap_gate(  9, &isr_unimpl);
-	set_trap_gate( 10, &isr_unimpl);
-	set_trap_gate( 11, &isr_unimpl);
-	set_trap_gate( 12, &isr_unimpl);
-	set_trap_gate( 13, &isr_unimpl);
-	set_trap_gate( 14, &isr_unimpl);
+	set_trap_gate(  9, &isr_coprocessor_segment_overrun);
+	set_trap_gate( 10, &isr_invalid_tss);
+	set_trap_gate( 11, &isr_segment_not_present);
+	set_trap_gate( 12, &isr_stack_segment_fault);
+	set_trap_gate( 13, &isr_general_protection_fault);
+	set_trap_gate( 14, &isr_page_fault);
 	set_trap_gate( 15, &isr_reserved);
-	set_trap_gate( 16, &isr_unimpl);
+	set_trap_gate( 16, &isr_x87_fpu_except);
 
 	/* ISR 15 and 17-32 : hardware reserved */
 	for (i = 17; i < 32; i++)
@@ -89,7 +84,7 @@ load_idt(void)
 
 
 void
-IRQ_mask(uint8_t line)
+irq_mask(uint8_t line)
 {
 	uint16_t port;
 	uint8_t value;
@@ -109,7 +104,7 @@ IRQ_mask(uint8_t line)
 
 
 void
-IRQ_unmask(uint8_t line)
+irq_unmask(uint8_t line)
 {
 	uint16_t port;
 	uint8_t value;
@@ -131,30 +126,133 @@ IRQ_unmask(uint8_t line)
 
 
 void
-do_timer(void)
+die(char * msg, unsigned long esp_ptr, long error_code)
 {
-	jiffies++;
-	if (jiffies % 1000 == 0)
-		printf("Timer Jiffies = %d\n", jiffies);
+	long * esp = (long *) esp_ptr;
+	printf(FG_COLOR_DARK_RED BG_COLOR_WHITE);
+	printf("%s: 0x%x\n", msg, error_code & 0xFFFF);
+	printf("EIP:    0x%x:0x%x\n"\
+		   "EFLAGS:    0x%x\n"\
+		   "ESP:    0x%x:0x%x\n",
+		esp[1], esp[0], esp[2], esp[4], esp[3]
+	);
+
+	__asm__ volatile ("cli"::);
+	while (1);
+}
+
+
+void do_divide_error(unsigned long esp, long error_code)
+{
+	die("Divide By Zero", esp, error_code);
 }
 
 
 void
-do_reserved(void)
+do_debug(unsigned long esp, long error_code)
 {
-	printf("Reserved\n");
+	die("Debug", esp, error_code);
+}
+
+
+void 
+do_nmi(unsigned long esp, long error_code)
+{
+	die("NMI", esp, error_code);
+}
+
+
+void do_int3(unsigned long esp, long error_code)
+{
+	die("Int 3 Break Point", esp, error_code);
 }
 
 
 void
-do_unimpl(void)
+do_overflow(unsigned long esp, long error_code)
 {
-	printf("unimplemented Trap\n");
+	die("Overflow\n", esp, error_code);
 }
 
 
 void
-do_double_fault(void)
+do_bounds(unsigned long esp, long error_code)
 {
-	printf("DOUBLE FAULT\n");
+	die("Bounds Range Exceeded", esp, error_code);
+}
+
+
+void
+do_invalid_op(unsigned long esp, long error_code)
+{
+	die("Invalid Operand", esp, error_code);
+}
+
+
+void
+do_device_unavailable(unsigned long esp, long error_code)
+{
+	die("Device Not Available\n", esp, error_code);
+}
+
+
+void
+do_double_fault(unsigned long esp, long error_code)
+{
+	die("DOUBLE FAULT", esp, error_code);
+}
+
+
+void
+do_coprocessor_segment_overrun(unsigned long esp, long error_code)
+{
+	die("Coprocessor Segment Overrun", esp, error_code);
+}
+
+
+void 
+do_invalid_tss(unsigned long esp, long error_code)
+{
+	die("Invalid TSS", esp, error_code);
+}
+
+void
+do_segment_not_present(unsigned long esp, long error_code)
+{
+	die("segment not present", esp, error_code);
+}
+
+
+void
+do_stack_segment_fault(unsigned long esp, long error_code)
+{
+	die("stack segment", esp, error_code);
+}
+
+
+void
+do_general_protection_fault(unsigned long esp, long error_code)
+{
+	die("General Protection Fault", esp, error_code);
+}
+
+
+void
+do_page_fault(unsigned long esp, long error_code)
+{
+	die("Page Fault", esp, error_code);
+}
+
+
+void
+do_reserved(unsigned long esp, long error_code)
+{
+	die("Reserved", esp, error_code);
+}
+
+
+void
+do_x87_fpu_except(unsigned long esp, long error_code)
+{
+	die("x87 FPU Exception", esp, error_code);
 }
--- fifos/fifos_preemptive/include/interrupt.h
+++ fifos/fifos_preemptive/include/interrupt.h
@@ -52,22 +52,55 @@ void init_idt(void);
 void load_idt(void);
 void idt_register_isr(uint8_t, void (*)(void), uint16_t, uint8_t);
 
-void IRQ_mask(uint8_t line);
-void IRQ_unmask(uint8_t line);
+void irq_mask(uint8_t line);
+void irq_unmask(uint8_t line);
 
-void eoi(uint8_t);
 
 /* ASM HANDLERS */
-extern void isr_timer(void);
-extern void isr_reserved(void);
-extern void isr_unimpl(void);
+extern void isr_divide_error(void);
+extern void isr_debug(void);
+extern void isr_nmi(void);
+extern void isr_int3(void);
+extern void isr_overflow(void);
+extern void isr_bounds(void);
+extern void isr_invalid_op(void);
+extern void isr_device_unavailable(void);
 extern void isr_double_fault(void);
+extern void isr_coprocessor_segment_overrun(void);
+extern void isr_invalid_tss(void);
+extern void isr_segment_not_present(void);
+extern void isr_stack_segment_fault(void);
+extern void isr_general_protection_fault(void);
+extern void isr_page_fault(void);
+extern void isr_reserved(void);
+extern void isr_x87_fpu_except(void);
+
+extern void isr_timer(void);
 extern void isr_kbd(void);
 
 /* C HANDLERS */
-void do_reserved(void);
-void do_unimpl(void);
-void do_double_fault(void);
+void do_divide_error(unsigned long, long);
+void do_debug(unsigned long, long);
+void do_nmi(unsigned long, long);
+void do_int3(unsigned long, long);
+void do_overflow(unsigned long, long);
+void do_bounds(unsigned long, long);
+void do_invalid_op(unsigned long, long);
+void do_device_unavailable(unsigned long, long);
+void do_double_fault(unsigned long, long);
+void do_coprocessor_segment_overrun(unsigned long, long);
+void do_invalid_tss(unsigned long, long);
+void do_segment_not_present(unsigned long, long);
+void do_stack_segment_fault(unsigned long, long);
+void do_general_protection_fault(unsigned long, long);
+void do_page_fault(unsigned long, long);
+void do_reserved(unsigned long, long);
+void do_x87_fpu_except(unsigned long, long);
+
+
+/* Inspired by Torwalds Kernel 0.99 */
+void die(char *, unsigned long, long);
+
 
 struct IDTDesc
 {
