70d85967ba36d74336e91d9c0a0cc651c94d91a5
thakkarv <thakkarv@bu.edu>
2018-04-17 10:06:23 -0400

    added a third test thread

--- fifos/fifos_preemptive/include/stateful_cr.h
+++ fifos/fifos_preemptive/include/stateful_cr.h
@@ -4,6 +4,7 @@
 /* routines run on threads */
 void stateful_cr_thread1(void);
 void stateful_cr_thread2(void);
+void stateful_cr_thread3(void);
 
 /* registers routines with correspoinding thread */
 void stateful_cr_register_routines(void);
--- fifos/fifos_preemptive/src/stateful_cr.c
+++ fifos/fifos_preemptive/src/stateful_cr.c
@@ -13,6 +13,7 @@ stateful_cr_thread1(void)
 
 	while (1)
 	{
+		// msleep(2);
 		/* execute instructions in this thread */
 		for (i = 0; i < 4; i++)
 		{
@@ -57,27 +58,51 @@ stateful_cr_thread2(void)
 
 
 void
+stateful_cr_thread3(void)
+{
+	int i;
+	int j = 0;
+	int counter = 0;
+
+	while (1)
+	{
+		/* execute instructions in this thread */
+		for (i = 0; i < 4; i++)
+		{
+			printf (" <3>:%d ", counter++);
+		}
+		printf ("\n");
+
+		/* check if done with this thread */
+		if (++j == 2)
+			break;
+	}
+
+	printf("Done <3>!\n");
+	return;
+}
+
+
+void
 stateful_cr_register_routines(void)
 {
 	/* create threads and register with scheduler */
 	tid_t thread1_id = thread_create(stateful_cr_thread1);
 	if (thread1_id == -1)
-	{
 		printf("\n    Could not create thread 1\n");
-	}
 	else
-	{
 		printf("\n    Thread 1 created with tid = %d\n", thread1_id);
-	}
 
 
 	tid_t thread2_id = thread_create(stateful_cr_thread2);
 	if (thread2_id == -1)
-	{
 		printf("    Could not create thread 2\n");
-	}
 	else
-	{
 		printf("    Thread 2 created with tid = %d\n", thread2_id);
-	}
+
+	tid_t thread3_id = thread_create(stateful_cr_thread3);
+	if (thread3_id == -1)
+		printf("    Could not create thread 3\n");
+	else
+		printf("    Thread 3 created with tid = %d\n", thread3_id);
 }
