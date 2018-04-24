8b2668439d3f3f6071f15db297e4f81e6efeb761
thakkarv <thakkarv@bu.edu>
2018-04-23 20:39:45 -0400

    initial commit for diskos branch

---
+++ fifos/fifos_preemptive/src/endoh.c
@@ -0,0 +1,57 @@
+// #include <stdio.h>//  .IOCCC                                         Fluid-  #
+// #include <unistd.h>  //2012                                         _Sim!_  #
+// #include <complex.h>  //||||                     ,____.              IOCCC-  #
+#include <kvideo.h>
+#include <sysutils.h>
+
+double complex	data[97687], *p, *q, *r = data, w = 0, d;
+int x, y;
+char calc_buffer[6856] = "\x1b[2J" "\x1b" "[1;1H     ";
+char *output_str = calc_buffer, *t;
+
+int
+endoh()
+{
+
+    // read video memory one by one
+	for (; 0 < (x = getc(stdin));)
+    {
+		w = x > 10 ? 32 < x ? 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9 : 0, w - I : (x = w + 2);;        
+    }
+
+	for (;; puts(output_str), output_str = calc_buffer + 4) {
+    {
+		for (p = data; p[2] = p[1] * 9, p < r; p += 5)
+        {
+			for (q = data; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5)
+            {
+				if (0 < (x = 1 - w))
+					p[2] += w * w;
+            }
+        }
+
+		for (p = data; p[3] = G, p < r; p += 5)
+        {
+			for (q = data; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5)
+            {
+				if (0 < (x = 1 - w))
+					p[3] += w * (d * (3 - p[2] - q[2]) * P + p[4] * V - q[4] * V) / p[2];
+            }
+        }
+
+		for (x = 011; 2012 - 1 > x++;)
+			calc_buffer[x] = 0;
+
+		for (p = data; (t = calc_buffer + 10 + (x = *p * I) + 80 * (y = *p / 2), *p += p[4] += p[3] / 10 * !p[1]), p < r; p += 5)
+        {
+			x = 0 <= x && x < 79 && 0 <= y && y < 23 ? 1[1[*t |= 8, t] |= 4, t += 80] = 1, *t |= 2 : 0;
+        }
+
+		for (x = 011; 2012 - 1 > x++;)
+			calc_buffer[x] = " '`-.|//,\\" "|\\_" "\\/\x23\n"[x % 80 - 9 ? x[calc_buffer] : 16];;
+
+		msleep(12321);
+	}
+    
+    return 0;
+}
