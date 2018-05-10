05e4aa48e2677cbfdec336cdce43e3ce7e44a3a0
VoidStar <thakkarv@bu.edu>
2018-05-10 09:15:57 -0400

    test file cleanup

--- DiskOS/test/rd_diskos_test.c
+++ DiskOS/test/rd_diskos_test.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>		/* memset */
-#include <unistd.h>		/* fork */
-#include "test_rd.h"
-// #include <sys/threads.h>
-// #include <stateful_cr.h>
-// #include <kvideo.h>
-// #include <kmalloc.h>
-// #include <ramdisk.h>
+#include <sys/threads.h>
+#include <sys/stdlib.h>
+#include <sysutils.h>
+#include <sys/ufs.h>
+#include <stateful_cr.h>
+#include <kvideo.h>
+#include <kmalloc.h>
+#include <ramdisk.h>
+#include <test_rd.h>
 
 /* we HAVE to use ramdisk */
 #define USE_RAMDISK
@@ -27,7 +26,7 @@
 #define TEST_SUCCESS 1
 #define TEST_FAILURE 0
 
-#define MAX_FILES 1023
+#define MAX_FILES 16
 #define BLK_SZ 256					/* Block size */
 #define DIRECT 8					/* Direct pointers in location attribute */
 #define PTR_SZ 4					/* 32-bit [relative] addressing */
@@ -44,36 +43,13 @@ static char data3[PTRS_PB*PTRS_PB*BLK_SZ]; 	/* Double indirect data size */
 static char addr[PTRS_PB*PTRS_PB*BLK_SZ+1]; /* Scratchpad memory */
 
 
-// FIXME: ramdisk.h inclusion throws error. solve after test framework is set up
-// FILE OPERATIONS
-int rd_create(char * param1) { return 1; }
-int rd_mkdir(char * param1) { return 1; }
-int rd_open(char * param1) { return 1; }
-int rd_close(int fd) { return 1; }
-int rd_read(int param3, char * param1, int param2) { return 1; }
-int rd_write(int param3, char * param1, int param2) { return 1; }
-int rd_lseek(int param3, int param4, int param2) { return 1; }
-int rd_unlink(char * param1) { return 1; }
-int rd_readdir(int param3, char * param1) { return 1; }
-static void int2str(int source, char *res, int num_digits);
-static int pow(int num, int exponent);
+// /* helper functions */
+// static void int2str(int source, char *res, int num_digits);
+static void * thread0(void * arg);
+static void * thread1(void * arg);
+// static int pow(int num, int exponent);
 
-
-int main()
-{
-	int status;
-	
-	status = run_tests();
-	if (status != TEST_SUCCESS)
-		printf("rd tests failed\n");
-	else
-		printf("rd tests passed\n");
-
-	return 0;
-}
-
-
-int run_tests(void)
+void * run_tests(void * arg)
 {
 	int i, retval;
 	int fd;
@@ -87,14 +63,15 @@ int run_tests(void)
 #ifdef TEST1
 
 	/* Generate MAXIMUM regular files */
-	for (i = 0; i < MAX_FILES + 1; i++) { // go beyond the limit
-		// TODO: set file name
-		// sprintf (pathname, PATH_PREFIX "/file%d", i);
-		printf("file num %d\n", i);
-
+	char c = '0';
+	for (i = 0; i < MAX_FILES + 1; i++)
+	{ // go beyond the limit
+		sprintf (pathname, PATH_PREFIX "/file");
+		pathname[5] = c++;
 		retval = rd_create (pathname);
 
-		if (retval < 0) {
+		if (retval < 0)
+		{
 			printf("rd_create: File creation error! status: %d (%s)\n",
 			retval, pathname);
 
@@ -107,12 +84,15 @@ int run_tests(void)
 	}   
 
 	/* Delete all the files created */
-	for (i = 0; i < MAX_FILES; i++) { 
-		// sprintf (pathname, PATH_PREFIX "/file%d", i);
-		printf("file num %d\n", i);
+	c = '0';	
+	for (i = 0; i < MAX_FILES; i++)
+	{ 
+		sprintf (pathname, PATH_PREFIX "/file");
+		pathname[5] = c++;		
 		retval = rd_unlink (pathname);
 
-		if (retval < 0) {
+		if (retval < 0)
+		{
 			printf ("unlink: File deletion error! status: %d\n", retval);
 			return TEST_FAILURE;
 			// return TEST_FAILURE; // exit(EXIT_FAILURE);
@@ -130,7 +110,8 @@ int run_tests(void)
 	/* Generate one LARGEST file */
 	retval = rd_create (PATH_PREFIX "/bigfile");
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf ("creat: File creation error! status: %d\n", retval);
 
 		return TEST_FAILURE;
@@ -139,7 +120,8 @@ int run_tests(void)
 
 	retval =  rd_open (PATH_PREFIX "/bigfile"); /* Open file to write to it */
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf ("open: File open error! status: %d\n", retval);
 
 		return TEST_FAILURE;
@@ -151,7 +133,8 @@ int run_tests(void)
 	/* Try writing to all direct data blocks */
 	retval = rd_write (fd, data1, sizeof(data1));
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf ("write: File write STAGE1 error! status: %d\n",
 		retval);
 
@@ -164,7 +147,8 @@ int run_tests(void)
 	/* Try writing to all single-indirect data blocks */
 	retval = rd_write (fd, data2, sizeof(data2));
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf ("write: File write STAGE2 error! status: %d\n",
 		retval);
 
@@ -177,7 +161,8 @@ int run_tests(void)
 	/* Try writing to all double-indirect data blocks */
 	retval = rd_write (fd, data3, sizeof(data3));
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf ("write: File write STAGE3 error! status: %d\n",
 		retval);
 
@@ -189,37 +174,47 @@ int run_tests(void)
 
 #endif // TEST_SINGLE_INDIRECT
 
+	printf("test2 ok\n");
+
 #endif // TEST2
 
-// TODO: replace rd_lseek parameter with SEEK_SET
 #ifdef TEST3
 
 	/* ****TEST 3: Seek and Read file test**** */
-	retval = rd_lseek (fd, 0, 0);	/* Go back to the beginning of your file */
+	retval = rd_lseek (fd, 0, SEEK_SET);	/* Go back to the beginning of your file */
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf("lseek: File seek error! status: %d\n",
 		retval);
 
 		return TEST_FAILURE; // exit(EXIT_FAILURE);
 	}
 
-	/* Try reading from all direct data blocks */
-	retval = rd_read (fd, addr, sizeof(data1));
+	char t[2];
 
-	if (retval < 0) {
+	/* Try reading from all direct data blocks */
+	retval = rd_read (fd, t, 1);
+
+	if (retval < 0)
+	{
 		printf("read: File read STAGE1 error! status: %d\n",
 		retval);
 
 		return TEST_FAILURE; // exit(EXIT_FAILURE);
 	}
+
+	t[1] = '\0';
+
 	/* Should be all 1s here... */
-	printf ("Data at addr: %s\n", addr);
+	// printf ("Data at addr: %s\n", t);
 
 #ifdef TEST_SINGLE_INDIRECT
 
+	retval = rd_lseek (fd, 5000, SEEK_SET);	/* Go back to the beginning of your file */
+
 	/* Try reading from all single-indirect data blocks */
-	retval = rd_read (fd, addr, sizeof(data2));
+	retval = rd_read (fd, t, 1);
 
 	if (retval < 0) {
 		printf("read: File read STAGE2 error! status: %d\n",
@@ -227,22 +222,25 @@ int run_tests(void)
 
 		return TEST_FAILURE; // exit(EXIT_FAILURE);
 	}
+
+	t[1] = '\0';
 	/* Should be all 2s here... */
-	printf ("Data at addr: %s\n", addr);
+	printf ("Data at addr: %s\n", t);
 
 #ifdef TEST_DOUBLE_INDIRECT
 
 	/* Try reading from all double-indirect data blocks */
 	retval = rd_read (fd, addr, sizeof(data3));
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf("read: File read STAGE3 error! status: %d\n",
 		retval);
 
 		return TEST_FAILURE; // exit(EXIT_FAILURE);
 	}
 	/* Should be all 3s here... */
-	printf ("Data at addr: %s\n", addr);
+	// printf ("Data at addr: %s\n", addr);
 
 #endif // TEST_DOUBLE_INDIRECT
 
@@ -251,7 +249,8 @@ int run_tests(void)
 	/* Close the bigfile */
 	retval = rd_close(fd);
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf("close: File close error! status: %d\n",
 		retval);
 
@@ -262,7 +261,8 @@ int run_tests(void)
 
 	retval = rd_unlink (PATH_PREFIX "/bigfile");
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf("unlink: /bigfile file deletion error! status: %d\n",
 		retval);
 
@@ -276,7 +276,8 @@ int run_tests(void)
 	/* ****TEST 4: Make directory and read directory entries**** */
 	retval = rd_mkdir (PATH_PREFIX "/dir1");
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf("mkdir: Directory 1 creation error! status: %d\n",
 		retval);
 
@@ -285,7 +286,8 @@ int run_tests(void)
 
 	retval = rd_mkdir (PATH_PREFIX "/dir1/dir2");
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf("mkdir: Directory 2 creation error! status: %d\n",
 		retval);
 
@@ -294,7 +296,8 @@ int run_tests(void)
 
 	retval = rd_mkdir (PATH_PREFIX "/dir1/dir3");
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf("mkdir: Directory 3 creation error! status: %d\n",
 		retval);
 
@@ -304,7 +307,8 @@ int run_tests(void)
 	#ifdef USE_RAMDISK
 	retval =  rd_open (PATH_PREFIX "/dir1"); /* Open directory file to read its entries */
 
-	if (retval < 0) {
+	if (retval < 0)
+	{
 		printf("open: Directory open error! status: %d\n",
 		retval);
 
@@ -315,88 +319,134 @@ int run_tests(void)
 
 	memset (addr, 0, sizeof(addr)); /* Clear scratchpad memory */
 
-	while ((retval = rd_readdir (fd, addr))) { /* 0 indicates end-of-file */
+	char readdirbuf[16];
 
-		if (retval < 0) {
+	int count = 0;
+	while (!(retval = rd_readdir (fd, readdirbuf)))
+	{
+		/* 0 indicates end-of-file */
+		if (++count == 3)
+			break;
+
+		if (retval < 0)
+		{
 			printf("readdir: Directory read error! status: %d\n",
 			retval);
 			return TEST_FAILURE; // exit(EXIT_FAILURE);
 		}
 
-		index_node_number = atoi(&addr[14]);
-		printf ("Contents at addr: [%s,%d]\n", addr, index_node_number);
+		// index_node_number = atoi(&addr[14]);
+		index_node_number = (&readdirbuf[14]);
+		printf("%s\n", readdirbuf);
+		printf ("Contents at addr: [%s,%s]\n", readdirbuf, index_node_number);
 	
 	}
 
 #endif // USE_RAMDISK
 #endif // TEST4
 
-// // TODO: test this fork "replacement"
-// #ifdef TEST5
+// TODO: test this fork "replacement"
+#ifdef TEST5
 
-// 	/* Create two threads and run rd create on both */
-// 	tid_t thread0_id = kthread_create(thread0, NULL);
-// 	if (thread0_id == -1)
-// 		printf("    Could not create thread 0\n");
-// 	else
-// 		printf("    Thread 0 created with tid = %d\n", thread0_id);
+	/* Create two threads and run rd create on both */
+	tid_t thread0_id = kthread_create(thread0, NULL);
+	if (thread0_id == -1)
+		printf("    Could not create thread 0\n");
+	else
+		printf("    Thread 0 created with tid = %d\n", thread0_id);
 
-// 	tid_t thread1_id = kthread_create(thread1, NULL);
-// 	if (thread1_id == -1)
-// 		printf("\n    Could not create thread 1\n");
-// 	else
-// 		printf("\n    Thread 1 created with tid = %d\n", thread1_id);
+	tid_t thread1_id = kthread_create(thread1, NULL);
+	if (thread1_id == -1)
+		printf("\n    Could not create thread 1\n");
+	else
+		printf("\n    Thread 1 created with tid = %d\n", thread1_id);
 
-// #endif // TEST5
+	__asm__ volatile("sti");
+
+#endif // TEST5
 
 	return TEST_SUCCESS;
 }
 
 
-// static void * thread0(void * arg)
-// {
-// 	int i, retval;
+static void * thread0(void * arg)
+{
+	int i, retval;
 
-// 	/* Generate 300 regular files */
-// 	for (i = 0; i < 300; i++) { 
-// 		sprintf (pathname, PATH_PREFIX "/file_t0_%d", i);
+	/* Generate 300 regular files */
+	for (i = 0; i < 1; i++)
+	{ 
+		// TODO: replace sprinft
+		sprintf (pathname, PATH_PREFIX "/file_t0");
 
-// 		retval = rd_create (pathname);
+		retval = rd_create (pathname);
 
-// 		if (retval < 0) {
-// 			printf("(thread0) create: File creation error! status: %d\n", 
-// 			retval);
-// 			return TEST_FAILURE; // exit(EXIT_FAILURE);
-// 		}
+		if (retval < 0)
+		{
+			printf("(thread0) create: File creation error! status: %d\n", 
+			retval);
+			return TEST_FAILURE; // exit(EXIT_FAILURE);
+		}
 
-// 		memset (pathname, 0, 80);
-// 	}
+		memset (pathname, 0, 80);
+	}
 
-// 	return;
-// }
+	msleep(200);
+
+	int fd;
+	fd = rd_open("/");
+
+	char readdirbuf[16];
+	int count = 0;
+	int index_node_number;
+	while (!(retval = rd_readdir (fd, readdirbuf)))
+	{ /* 0 indicates end-of-file */
+
+		if (++count == 3)
+			break;
+
+		if (retval < 0)
+		{
+			printf("readdir: Directory read error! status: %d\n",
+			retval);
+			return TEST_FAILURE; // exit(EXIT_FAILURE);
+		}
+
+		// index_node_number = atoi(&addr[14]);
+		index_node_number = (&readdirbuf[14]);
+		printf("%s\n", readdirbuf);
+		printf ("Contents at addr: [%s,%s]\n", readdirbuf, index_node_number);
+	
+	}
+
+	return 0;
+}
 
 
-// static void * thread1(void * arg)
-// {
-// 	int i, retval;
+static void * thread1(void * arg)
+{
+	int i, retval;
 
-// 	/* Generate 300 regular files */
-// 	for (i = 0; i < 300; i++) { 
-// 		sprintf (pathname, PATH_PREFIX "/file_t1_%d", i);
+	// msleep(100);
+	/* Generate 300 regular files */
+	for (i = 0; i < 1; i++)
+	{ 
+		sprintf (pathname, PATH_PREFIX "/file_t1");
 
-// 		retval = rd_create (pathname);
+		retval = rd_create (pathname);
 
-// 		if (retval < 0) {
-// 			printf("(thread1) create: File creation error! status: %d\n", 
-// 			retval);
-// 			return TEST_FAILURE; // exit(EXIT_FAILURE);
-// 		}
+		if (retval < 0)
+		{
+			printf("(thread1) create: File creation error! status: %d\n", 
+			retval);
+			return TEST_FAILURE; // exit(EXIT_FAILURE);
+		}
 
-// 		memset (pathname, 0, 80);
-// 	}
+		memset (pathname, 0, 80);
+	}
 
-// 	return;
-// }
+	return 0;
+}
 
 
 /**
@@ -406,22 +456,22 @@ int run_tests(void)
  */
 
 
-static int pow(int num, int exponent)
-{
-	int res = 1;
-	int i;
-	for (i = 0; i < exponent; ++i)
-		res *= num;
+// static int pow(int num, int exponent)
+// {
+// 	int res = 1;
+// 	int i;
+// 	for (i = 0; i < exponent; ++i)
+// 		res *= num;
 
-	return res;
-}
+// 	return res;
+// }
 
 
-static void int2str(int source, char * res, int num_digits)
-{
-	int i;
-	for (i = 0; i < num_digits; ++i)
-		res[i] = (source%pow(10,num_digits-i) / pow(10,num_digits-i-1)) + 0x30;
+// static void int2str(int source, char * res, int num_digits)
+// {
+// 	int i;
+// 	for (i = 0; i < num_digits; ++i)
+// 		res[i] = (source%pow(10,num_digits-i) / pow(10,num_digits-i-1)) + 0x30;
 
-	res[num_digits] = '\0';
-}
+// 	res[num_digits] = '\0';
+// }
